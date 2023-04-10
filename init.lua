0d0f0dc54b122b7326aaafc2a2c4edc9bc53d395
김지훈 <jihoonkim@itsdcode.com>
2023-04-10 15:08:51 +0900

    feat : shift + space로 한/영 전환

--- init.lua
+++ init.lua
@@ -220,6 +220,30 @@ end
 
 require('modules.inputsource_aurora')
 
+-- plugInstall()
+
+do  -- input sorce changer
+    local inputSource = {
+        english = "com.apple.keylayout.ABC",
+        korean = "com.apple.inputmethod.Korean.2SetKorean",
+    }
+
+    local changeInput = function()
+
+        local current = hs.keycodes.currentSourceID()
+        local nextInput = nil
+
+        if current == inputSource.english then
+            nextInput = inputSource.korean
+        else
+            nextInput = inputSource.english
+        end
+        hs.keycodes.currentSourceID(nextInput)
+    end
+
+    hs.hotkey.bind({'shift'}, 'space', changeInput)
+end
+
 
 require('modules.focus-screen')
 
